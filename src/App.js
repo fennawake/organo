7ce0f375043f96eb75bf46ba81aaa979e358cae4
Luiz Fernando Souza <fenna.wake@gmail.com>
2023-06-27 15:46:34 +1000

    Add UID to project

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from './components/Banner';
 import Form from './components/Form';
 import Team from './components/Team';
 import Footer from './components/Footer';
+import { uid } from 'uid';
 
 function App() {
 
   const [teams, setTeams] = useState([
     {
+      id: uid(),
       name: 'Programming',
       color: '#D9F7E9'
     },
     {
+      id: uid(),
       name: 'Front-end',
       color: '#E8F8FF'
     },
     {
+      id: uid(),
       name: 'Data Science',
       color: '#F0F8E2'
     },
     {
+      id: uid(),
       name: 'Devops',
       color: '#FDE7E8'
     },
     {
+      id: uid(),
       name: 'UX & Design',
       color: '#FAE9F5'
     },
     {
+      id: uid(),
       name: 'Mobile',
       color: '#FFF5D9'
     },
     {
+      id: uid(),
       name: 'Innovation and Management',
       color: '#FFEEDF'
     }
@@ -39,144 +47,168 @@ function App() {
 
   const inicial = [
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[0].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[0].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[0].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Hipster and CEO at Alura',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
       team: teams[0].name
     },
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[1].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[1].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[1].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
       team: teams[1].name
     },
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[2].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[2].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[2].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
       team: teams[2].name
     },
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[3].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[3].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[3].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
       team: teams[3].name
     },
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[4].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[4].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[4].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
       team: teams[4].name
     },
     {
+      id: uid(),
       name: 'JULIANA AMOASEI',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
       team: teams[5].name
     },
     {
+      id: uid(),
       name: 'DANIEL ARTINE',
       position: 'Software Engineer at Stone Age',
       image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
       team: teams[5].name
     },
     {
+      id: uid(),
       name: 'GUILHERME LIMA',
       position: 'Python and JavaScript developer at Alura',
       image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
       team: teams[5].name
     },
     {
+      id: uid(),
       name: 'PAULO SILVEIRA',
       position: 'Software developer and instructor',
       image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
@@ -194,9 +226,9 @@ function App() {
     console.log('deleting collaborator...')
   }
 
-  const changeTeamColor = (color, name) => {
+  const changeTeamColor = (color, id) => {
     setTeams(teams.map(team => {
-      if (team.name === name) {
+      if (team.id === id) {
         team.color = color
       }
       return team
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -5,10 +5,10 @@ import hexToRgba from 'hex-to-rgba';
 const Team = ({ team, collaborators, deleteCollaborator, changeColor }) => {
     return (
         collaborators.length > 0 && <section className='team' style={{ backgroundImage: 'url(/organo/image/fundo.png)', backgroundColor: hexToRgba(team.color, '0.6') }}>
-            <input onChange={event => changeColor(event.target.value, team.name)} value={team.cor} type='color' className='input-color' />
-            <h3 style={{ color: team.cor, borderColor: team.cor }}>{team.name}</h3>
+            <input onChange={event => changeColor(event.target.value, team.id)} value={team.color} type='color' className='input-color' />
+            <h3 style={{ borderColor: team.color }}>{team.name}</h3>
             <div className='collaborators'>
-                {collaborators.map((collaborator, index) => <Contributor key={index} collaborator={collaborator} backgroundColor={team.cor} deleteCollaborator={deleteCollaborator} />
+                {collaborators.map((collaborator, index) => <Contributor key={index} collaborator={collaborator} backgroundColor={team.color} deleteCollaborator={deleteCollaborator} />
                 )}
             </div>
         </section>
