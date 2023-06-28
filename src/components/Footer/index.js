03572dbe47c95d1792e056647af3c85605dfe596
Luiz Fernando Souza <fenna.wake@gmail.com>
2023-06-28 15:36:21 +1000

    Change image footer path and alignment of collaborators

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,12 +6,12 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div className='social'>
-                <a href='#'><img src="./organo/image/fb.png" /></a>
-                <a href='#'><img src="./organo/image/tw.png" /></a>
-                <a href='#'><img src="./organo/image/ig.png" /></a>
+                <a href='#'><img src="./image/fb.png" /></a>
+                <a href='#'><img src="./image/tw.png" /></a>
+                <a href='#'><img src="./image/ig.png" /></a>
             </div>
             <div className='logo'>
-                <img src="./organo/image/logo.png" />
+                <img src="./image/logo.png" />
             </div>
             <div className='developed'>
                 <p>Developed by Luiz Fernando Souza</p>
